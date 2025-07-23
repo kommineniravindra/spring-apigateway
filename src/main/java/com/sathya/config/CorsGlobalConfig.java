0d00c812cffea7625bcd0bad48243001de32e3aa
kommineniravindra <kommineniravindra99@gmail.com>
2025-07-23 17:45:57 +0530

    update

--- src/main/java/com/sathya/config/CorsGlobalConfig.java
+++ src/main/java/com/sathya/config/CorsGlobalConfig.java
@@ -23,7 +23,7 @@ public class CorsGlobalConfig {
         ));
 
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
